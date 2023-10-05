2e61c020d86c10e5ef032d8ecad5fe0cc5c94276
Joshua Hindley <joshua.hindley2222@gmail.com>
2023-10-05 17:07:22 +1300

    Add functionality for importing and exporting JSON

--- main.js
+++ main.js
@@ -32,6 +32,17 @@ var ratingToColourMap = {
   "nzf": "#000000",
 }
 
+var colourToRatingMap = {
+  "#767171": "close-race",
+  "#d00a12": "lab",
+  "#1e4882": "nat",
+  "#0ca747": "grn",
+  "#f3ca1e": "act",
+  "#c37d23": "tpm",
+  "#09B598": "top",
+  "#000000": "nzf",
+}
+
 async function handlePageLoad() {
   await fetch('./2020-results.svg').then(resp => resp.text()).then(text => document.getElementById('svg').innerHTML = text)
 
@@ -113,6 +124,96 @@ function showHelpModal() {
 
 function goToTextTool() {
   document.location.href = "https://hindley.me/interactive-electorate-ratings/"
+}
+
+function onChooseFile(event) {
+  const files = event.target?.files
+
+  if (!files || typeof window.FileReader !== 'function') {
+    alert("Sorry, your browser does not support this functionality.")
+    console.error("Sorry, your browser does not support this functionality.")
+    const elem = document.getElementById("import-ratings")
+    elem.className = "button disabled"
+    elem.onclick = null
+  } else if (files[0]) {
+    const fr = new FileReader()
+    fr.onload = val => {
+      loadRatings(JSON.parse(val.target.result))
+    }
+    fr.readAsText(files[0])
+  } else {
+    return undefined
+  }
+
+  // reset the file to null so the same file can be selected again
+  document.getElementById("import-json").value = null
+}
+
+const rgbToHex = (r, g, b) => {
+  return '#' + [r, g, b].map(x => {
+    const hex = x.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+  }).join('')
+}
+
+function loadRatings(ratings) {
+
+  var pathElems = document.querySelectorAll("path")
+  backupRatings = {}
+  
+  for (var pathElem of pathElems) {
+    const id = pathElem.getAttribute('inkscape:label')
+    if (id !== null) {
+      const rgb = pathElem.style.fill.slice(4, pathElem.style.fill.length - 1).split(', ')
+      backupRatings[id] = colourToRatingMap[rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]))]
+    }
+  }
+
+  for (var pathElem of pathElems) {
+    const id = pathElem.getAttribute('inkscape:label')
+    if (id !== null) {
+      if (!ratings[id]) {
+        alert(`Electorate with id "${id}" missing from JSON file.`)
+        console.error(`Electorate with id "${id}" missing from JSON file.`)
+        loadRatings(backupRatings)
+        return
+      } else if (!Object.keys(ratingToColourMap).includes(ratings[id])) {
+        alert(`Invalid rating "${ratings[id]}" for electorate with id "${id}".\n\nThe valid ratings are "close-race", "nat", "lab", "act", "grn", "tpm", "nzf", and "top".`)
+        console.error(`Invalid rating "${ratings[id]}" for electorate with id "${id}".\n\nThe valid ratings are "close-race", "nat", "lab", "act", "grn", "tpm", "nzf", and "top".`)
+        loadRatings(backupRatings)
+        return
+      } else {
+        pathElem.style.fill = ratingToColourMap[ratings[id]]
+      }
+    }
+  }
+}
+
+function importJson() {
+  document.getElementById("import-json").click()
+}
+
+function exportJson() {
+  // TODO sort this
+  var pathElems = document.querySelectorAll("path")
+  var json = {}
+
+  for (var pathElem of pathElems) {
+    const id = pathElem.getAttribute('inkscape:label')
+    if (id !== null) {
+      const rgb = pathElem.style.fill.slice(4, pathElem.style.fill.length - 1).split(', ')
+      json[id] = colourToRatingMap[rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]))]
+    }
+  }
+
+  const dict = JSON.stringify(json)
+  const blob = new Blob([dict], { type: "application/json"})
+  const url = window.URL.createObjectURL(blob)
+
+  const a = document.createElement("a")
+  a.href = url
+  a.download = "electorate-ratings.json"
+  a.click()
 }
 
 function capture() {
